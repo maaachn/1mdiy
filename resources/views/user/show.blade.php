025f10fcadb523a584a8c629a0d7e2d4989ff94a
tsukasa.nakashima <tsukasa.nakashima@rakuten.com>
2018-07-10 05:37:44 +0000

    usershow

---
+++ resources/views/user/show.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <aside class="col-xs-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">{{ $user->name }}</h3>
+                </div>
+                <div class="panel-body">
+                  <img class="media-object img-rounded img-responsive" src="{{ Gravatar::src($user->name, 500) }}" alt="">
+                </div>
+            </div>
+        </aside>
+        <div class="col-xs-8">
+            
+            @include('item.item', ['items' => $items])
+        </div>
+        
+    </div>
+@endsection
