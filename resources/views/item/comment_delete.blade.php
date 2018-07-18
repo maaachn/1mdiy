73fac1bde80461b26cdc84606647320d53b37fd8
kota-yamada <lec.20th@gmail.com>
2018-07-18 06:57:37 +0000

    trash1.jpeg

--- resources/views/item/comment_delete.blade.php
+++ resources/views/item/comment_delete.blade.php
@@ -1,6 +1,6 @@
 @if (Auth::user()->id == $comment->user_id)
     {!! Form::open(['route' => ['comments.destroy', $comment->id], 'method' => 'delete']) !!}
-        {!! Form::image('img/trash.jpeg', 'btnSub', ['class' => 'btn']) !!}
+        {!! Form::image('img/trash1.jpeg', 'btnSub', ['class' => 'btn']) !!}
         <!--@if(isset($redirectUrl))-->
         <!--<input type='hidden' name="redirectUrl" value='{{urlencode($redirectUrl)}}'>-->
         <!--@endif-->
--- resources/views/item/delete_button.blade.php
+++ resources/views/item/delete_button.blade.php
@@ -1,6 +1,6 @@
 @if (Auth::user()->id == $item->user_id)
     {!! Form::open(['route' => ['works.destroy', $item->id], 'method' => 'delete']) !!}
-        {!! Form::image('img/trash.jpeg', 'btnSub', ['class' => 'btn']) !!}
+        {!! Form::image('img/trash1.jpeg', 'btnSub', ['class' => 'btn']) !!}
         <!--@if(isset($redirectUrl))-->
         <!--<input type='hidden' name="redirectUrl" value='{{urlencode($redirectUrl)}}'>-->
         <!--@endif-->
