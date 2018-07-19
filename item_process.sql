574b79135da3a0e5f58ee04913d558916e67aa94
yuina.fukuoka <yuina.fukuoka@rakuten.com>
2018-07-19 05:04:18 +0000

    update data 1

--- item_process.sql
+++ item_process.sql
@@ -3,11 +3,11 @@ INSERT INTO 1mdiy.item_process (item_id, process1, process2, process3, process4,
 INSERT INTO 1mdiy.item_process (item_id, process1, process2, process3, process4, process5) VALUES (3,'item_photo/candle/Candle2.jpg','item_photo/candle/Candle3.jpg','item_photo/candle/Candle4.jpg','','');
 INSERT INTO 1mdiy.item_process (item_id, process1, process2, process3, process4, process5) VALUES (4,'item_photo/tissuecover/Tissuecover2.jpg','item_photo/ tissuecover/Tissuecover3.jpg','','','');
 INSERT INTO 1mdiy.item_process (item_id, process1, process2, process3, process4, process5) VALUES (5,'','','','','');
-INSERT INTO 1mdiy.item_process (item_id, process1, process2, process3, process4, process5) VALUES (6,'item_photo/binlight /binlight_recipe.jpg','item_photo/binlight/binlight_recipe2.jpg','','','');
+INSERT INTO 1mdiy.item_process (item_id, process1, process2, process3, process4, process5) VALUES (6,'item_photo/binlight/binlight_recipe.jpg','item_photo/binlight/binlight_recipe2.jpg','','','');
 INSERT INTO 1mdiy.item_process (item_id, process1, process2, process3, process4, process5) VALUES (7,'item_photo/smpstand/stand_recipe.jpg','item_photo/smpstand/stand_recipe2.jpg','','','');
 INSERT INTO 1mdiy.item_process (item_id, process1, process2, process3, process4, process5) VALUES (8,'item_photo/paperholder/paperholder_recipe.jpg',' paperholder/paperholder_image.jpg','','','');
 INSERT INTO 1mdiy.item_process (item_id, process1, process2, process3, process4, process5) VALUES (9,'item_photo/codecover/codecover01.jpg','item_photo/codecover/code cover02.jpg','','','');
-INSERT INTO 1mdiy.item_process (item_id, process1, process2, process3, process4, process5) VALUES (10,'','','','','');
+INSERT INTO 1mdiy.item_process (item_id, process1, process2, process3, process4, process5) VALUES (10,'item_photo/glass/glass_recipe.jpg','item_photo/glass/glass_recipe2.jpg','','','');
 INSERT INTO 1mdiy.item_process (item_id, process1, process2, process3, process4, process5) VALUES (11,'item_photo/cap/cap_recipe.jpg','item_photo/cap/cap_recipe1.jpg','','','');
 INSERT INTO 1mdiy.item_process (item_id, process1, process2, process3, process4, process5) VALUES (12,'item_photo/codecase/code_recipe.jpg','item_photo/codecase/code_recipe2.jpg','item_photo/codecase/code_recipe3.jpg','item_photo/codecase/code_recipe4.jpg','');
 INSERT INTO 1mdiy.item_process (item_id, process1, process2, process3, process4, process5) VALUES (13,'item_photo/hikidashi/boxtissue_recipe.jpg','item_photo/hikidashi/boxtissue_recipe2.jpg','item_photo/hikidashi/boxtissue_recipe3.jpg','item_photo/hikidashi/boxtissue_recipe4.jpg','');
@@ -20,5 +20,5 @@ INSERT INTO 1mdiy.item_process (item_id, process1, process2, process3, process4,
 INSERT INTO 1mdiy.item_process (item_id, process1, process2, process3, process4, process5) VALUES (20,'item_photo/drypopuri/Drypopuri2.jpg','item_photo/drypopuri/Drypopuri3.jpg','','','');
 INSERT INTO 1mdiy.item_process (item_id, process1, process2, process3, process4, process5) VALUES (21,'item_photo/projector/projector_recipe.jpg','item_photo/projector/projector_recipe2.jpg','item_photo/projector/projector_recipe6.jpg','item_photo/projector/projector_recipe4.jpg','item_photo/projector/projector_recipe5.jpg');
 INSERT INTO 1mdiy.item_process (item_id, process1, process2, process3, process4, process5) VALUES (22,'','','','','');
-INSERT INTO 1mdiy.item_process (item_id, process1, process2, process3, process4, process5) VALUES (23,'item_photo/petlight/prtlight_recipe.jpg','item_photo/petlight/prtlight_recipe2.jpg','','','');
+INSERT INTO 1mdiy.item_process (item_id, process1, process2, process3, process4, process5) VALUES (23,'item_photo/petlight/petlight_recipe.jpg','item_photo/petlight/petlight_recipe2.jpg','','','');
 INSERT INTO 1mdiy.item_process (item_id, process1, process2, process3, process4, process5) VALUES (24,'item_photo/fragrance/Fragrance1.jpg','item_photo/fragrance/Fragrance2.jpg','item_photo/fragrance/Fragrance3.jpg','','');
