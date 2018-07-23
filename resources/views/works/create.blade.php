c8aa77c2fa67b98d835fed8f71a838a4f8f3e4d4
kota-yamada <lec.20th@gmail.com>
2018-07-23 05:59:07 +0000

    checkbox

--- resources/views/works/create.blade.php
+++ resources/views/works/create.blade.php
@@ -45,66 +45,84 @@
                     {!! Form::label('name', '材料1') !!}
                     {!! Form::text('material1', old('material1'), ['class' => 'form-control', 'placeholder' => '（例）びん、段ボールなど']) !!}
                 </div>
-                <div class="form-group">
-                    {!! Form::label('name', '材料2（任意）') !!}
-                    {!! Form::text('material2', old('material2'), ['class' => 'form-control']) !!}
-                </div>
-                <div class="form-group">
-                    {!! Form::label('name', '材料3（任意）') !!}
-                    {!! Form::text('material3', old('material3'), ['class' => 'form-control']) !!}
-                </div>
-                <div class="form-group">
-                    {!! Form::label('name', '材料4（任意）') !!}
-                    {!! Form::text('material4', old('material3'), ['class' => 'form-control']) !!}
-                </div>
-                <div class="form-group">
-                    {!! Form::label('name', '材料5（任意）') !!}
-                    {!! Form::text('material5', old('material3'), ['class' => 'form-control']) !!}
-                </div>
+                <div class="hidden_zairyou">
+                    <label for="label1" class="checkbox">材料を追加する</label>
+                    <input type="checkbox" id="label1"/>
+                    <div class="zairyou">
+                        <div class="form-group">
+                            {!! Form::label('name', '材料2（任意）') !!}
+                            {!! Form::text('material2', old('material2'), ['class' => 'form-control']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::label('name', '材料3（任意）') !!}
+                            {!! Form::text('material3', old('material3'), ['class' => 'form-control']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::label('name', '材料4（任意）') !!}
+                            {!! Form::text('material4', old('material3'), ['class' => 'form-control']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::label('name', '材料5（任意）') !!}
+                            {!! Form::text('material5', old('material3'), ['class' => 'form-control']) !!}
+                        </div>
+                    </div>
+                </div>    
             </div>
             <div class="col-xs-12">
                 <div class="form-group">    
                     {!! Form::label('name', '手順1') !!}
                     {!! Form::textarea('recipe1', old('recipe1'), ['class' => 'form-control', 'rows' => '2', 'placeholder' => '（例）段ボールを15㎝四方に切ります']) !!}
                 </div>
-                <div class="form-group">    
-                    {!! Form::label('name', '手順2（任意）') !!}
-                    {!! Form::textarea('recipe2', old('recipe2'), ['class' => 'form-control', 'rows' => '2']) !!}
-                </div>
-                <div class="form-group">    
-                    {!! Form::label('name', '手順3（任意）') !!}
-                    {!! Form::textarea('recipe3', old('recipe3'), ['class' => 'form-control', 'rows' => '2']) !!}
-                </div>
-                <div class="form-group">    
-                    {!! Form::label('name', '手順4（任意）') !!}
-                    {!! Form::textarea('recipe4', old('recipe4'), ['class' => 'form-control', 'rows' => '2']) !!}
-                </div>
-                <div class="form-group">    
-                    {!! Form::label('name', '手順5（任意）') !!}
-                    {!! Form::textarea('recipe5', old('recipe5'), ['class' => 'form-control', 'rows' => '2']) !!}
-                </div>
+                <div class="hidden_tejun">
+                    <label for="label2" class="checkbox">材料を追加する</label>
+                    <input type="checkbox" id="label2"/>
+                    <div class="tejun">
+                        <div class="form-group">    
+                            {!! Form::label('name', '手順2（任意）') !!}
+                            {!! Form::textarea('recipe2', old('recipe2'), ['class' => 'form-control', 'rows' => '2']) !!}
+                        </div>
+                        <div class="form-group">    
+                            {!! Form::label('name', '手順3（任意）') !!}
+                            {!! Form::textarea('recipe3', old('recipe3'), ['class' => 'form-control', 'rows' => '2']) !!}
+                        </div>
+                        <div class="form-group">    
+                            {!! Form::label('name', '手順4（任意）') !!}
+                            {!! Form::textarea('recipe4', old('recipe4'), ['class' => 'form-control', 'rows' => '2']) !!}
+                        </div>
+                        <div class="form-group">    
+                            {!! Form::label('name', '手順5（任意）') !!}
+                            {!! Form::textarea('recipe5', old('recipe5'), ['class' => 'form-control', 'rows' => '2']) !!}
+                        </div>
+                    </div>
+                </div>    
             </div>
             <div class="col-xs-6">
                 <div class="form-group">    
                     {!! Form::label('name', '必要な道具1') !!}
                     {!! Form::text('tool1', old('tool1'), ['class' => 'form-control', 'placeholder' => '（例）はさみ']) !!}
                 </div>
-                <div class="form-group">    
-                    {!! Form::label('name', '必要な道具2（任意）') !!}
-                    {!! Form::text('tool2', old('tool2'), ['class' => 'form-control']) !!}
-                </div>
-                <div class="form-group">    
-                    {!! Form::label('name', '必要な道具3（任意）') !!}
-                    {!! Form::text('tool3', old('tool3'), ['class' => 'form-control']) !!}
-                </div>
-                <div class="form-group">    
-                    {!! Form::label('name', '必要な道具4（任意）') !!}
-                    {!! Form::text('tool4', old('tool4'), ['class' => 'form-control']) !!}
-                </div>
-                <div class="form-group">    
-                    {!! Form::label('name', '必要な道具5（任意）') !!}
-                    {!! Form::text('tool5', old('tool5'), ['class' => 'form-control']) !!}
-                </div>
+                <div class="hidden_douguu">
+                    <label for="label3" class="checkbox">材料を追加する</label>
+                    <input type="checkbox" id="label3"/>
+                    <div class="dougu">
+                        <div class="form-group">    
+                            {!! Form::label('name', '必要な道具2（任意）') !!}
+                            {!! Form::text('tool2', old('tool2'), ['class' => 'form-control']) !!}
+                        </div>
+                        <div class="form-group">    
+                            {!! Form::label('name', '必要な道具3（任意）') !!}
+                            {!! Form::text('tool3', old('tool3'), ['class' => 'form-control']) !!}
+                        </div>
+                        <div class="form-group">    
+                            {!! Form::label('name', '必要な道具4（任意）') !!}
+                            {!! Form::text('tool4', old('tool4'), ['class' => 'form-control']) !!}
+                        </div>
+                        <div class="form-group">    
+                            {!! Form::label('name', '必要な道具5（任意）') !!}
+                            {!! Form::text('tool5', old('tool5'), ['class' => 'form-control']) !!}
+                        </div>
+                    </div>
+                </div>    
             </div>
             <div class="form-group">    
                 {!! Form::submit('Create', ['class' => 'btn btn-primary btn-block']) !!}
