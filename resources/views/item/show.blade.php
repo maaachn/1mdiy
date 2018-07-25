08d9d9cbeddd5abeb6f7dab0b625882e864a6c28
yuina.fukuoka <yuina.fukuoka@rakuten.com>
2018-07-25 00:58:47 +0000

    modify no-image

--- resources/views/item/show.blade.php
+++ resources/views/item/show.blade.php
@@ -55,54 +55,69 @@
             <div class="status text-center">
                 <h2>作り方</h2>
                 <div class="form-group col-xs-4">
+                @if(isset($item) && $item->recipe1 != '')
                     @if(isset($process) && $process->process1 != '')
                     <div class="form-heading text-center"><img src='{{ secure_asset($process->process1) }}' alt=''></div>
+                    @elseif(isset($process) && $process->process1 == '')
+                    <div class="form-heading text-center"></div>
                     @else
                     <div class="form-heading text-center"><img src="/img/no_image.png" alt=''></div>
                     @endif
-                    @if(isset($item) && $item->recipe1 != '')
+                    
                     <div class="form-body text-center"><p>1.{{ $item->recipe1 }}</p></div>
-                    @endif
+                @endif
                 </div>
                 <div class="form-group col-xs-4">
+                @if(isset($item) && $item->recipe2 != '')
                    @if(isset($process) && $process->process2 != '')
                     <div class="form-heading text-center"><img src='{{ secure_asset($process->process2) }}' alt=''></div>
+                    @elseif(isset($process) && $process->process2 == '')
+                    <div class="form-heading text-center"></div>
                     @else
                     <div class="form-heading text-center"><img src="/img/no_image.png" alt=''></div>
                     @endif
-                    @if(isset($item) && $item->recipe2 != '')
+                    
                     <div class="form-body text-center"><p>2.{{ $item->recipe2 }}</p></div>
-                    @endif
+                @endif
                 </div>
                 <div class="form-group col-xs-4">
+                @if(isset($item) && $item->recipe3 != '')
                    @if(isset($process) && $process->process3 != '')
                     <div class="form-heading text-center"><img src='{{ secure_asset($process->process3) }}' alt=''></div>
-                     @else
+                    @elseif(isset($process) && $process->process3 == '')
+                    <div class="form-heading text-center"></div>
+                    @else
                     <div class="form-heading text-center"><img src="/img/no_image.png" alt=''></div>
                     @endif
-                    @if(isset($item) && $item->recipe3 != '')
+                    
                     <div class="form-body text-center"><p>3.{{ $item->recipe3 }}</p></div>
-                    @endif
+                @endif
                 </div>
                 <div class="form-group col-xs-4">
+                @if(isset($item) && $item->recipe4 != '')
                     @if(isset($process) && $process->process4 != '')
                     <div class="form-heading text-center"><img src='{{ secure_asset($process->process4) }}' alt='test'></div>
-                     @else
+                    @elseif(isset($process) && $process->process4 == '')
+                    <div class="form-heading text-center"></div>
+                    @else
                     <div class="form-heading text-center"><img src="/img/no_image.png" alt=''></div>
                     @endif
-                    @if(isset($item) && $item->recipe4 != '')
+                    
                     <div class="form-body text-center"><p>4.{{ $item->recipe4 }}</p></div>
-                    @endif
+                @endif
                 </div>
                 <div class="form-group col-xs-4">
+                @if(isset($item) && $item->recipe5 != '')
                     @if(isset($process) && $process->process5 != '')
                     <div class="form-heading text-center"><img src='{{ secure_asset($process->process5) }}' alt='test'></div>
+                    @elseif(isset($process) && $process->process5 == '')
+                    <div class="form-heading text-center"></div>
                     @else
                    <div class="form-heading text-center"><img src="/img/no_image.png" alt=''></div>
                     @endif
-                    @if(isset($item) && $item->recipe5 != '')
+                    
                     <div class="form-body text-center"><p>5.{{ $item->recipe5 }}</p></div>
-                    @endif
+                @endif
                 </div>
             </div>
         </div>
