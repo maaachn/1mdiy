e37c6b04837282b8c320f5f9530bfd215043fbee
kota-yamada <lec.20th@gmail.com>
2018-07-31 02:52:28 +0000

    link to jump

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,37 +8,31 @@
                 <div class="cover-contents row col-xs-12">
                     <div class="main-movie clearfix col-xs-12">
                         <video class="col-xs-12" poster="" id="bgvid" playsinline autoplay muted loop>
-
                         <source src="toppagetest.mp4" type="video/mp4">
                         </video>
-                       
                     </div>
-                        
                         <div id="auth" class="overlay">
-                            <div class="title">
-                                <!--<p>-クラフトマン-</p>-->
-                                <div class="logo">
                                     <img src="logo53.png">
-                                </div>
-                            </div>
                         </div>
-                            
-                         
-                         
-                         </div>
-                        </div>
-             
-                    </div>
                 </div>
-                
             </div>    
         </div>
     </div>
-    <div class="row">
+    <div class="col-xs-12">
+        <p>　</p>
+        <p>　</p>
+    </div>    
     
-    <h1 class="text-center">空き箱、紙袋、なんかためちゃう……</h1>
+<div class="row">
+    <div　class="form-group col-xs-6 col-sm-6">
+        <div class="form-body text-center">
+            <a href="#test" ><button class="submit">早速始める</button></a>
+        </div>   
+    </div>   
+    <p>　</p>
+        <p>　</p>
+    <h1 class="text-center" id="test">空き箱、紙袋、なんかためちゃう……</h1>
     <h1 class="text-center">困ってるなら作っちゃおう！</h1>
-    
     <div　class="form-group col-xs-6 col-sm-6">
         <div class="form-hedding text-center">
             <h2>探す</h2>
@@ -50,8 +44,6 @@
             <a href="{{ route('materials.get') }}" ><button class="submit">作品一覧へ</button></a>
         </div>
     </div>
-    
-    
     <div　class="form-group col-xs-6 col-sm-6">
         <div class="form-hedding text-center">
             <h2>作る</h2>
@@ -63,6 +55,7 @@
             <a href="{{ route('works.create') }}" ><button class="submit">投稿してみる</button></a>
         </div>
     </div>
+    
 </div>    
 @endsection
 
